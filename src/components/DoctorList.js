1e77db2f3c1cf22d5480a551974d94427e34c7af
Kevin Bai <lkevin957@gmail.com>
2023-11-19 10:00:02 -0800

    doctor list

---
+++ src/components/DoctorList.js
@@ -0,0 +1,20 @@
+// src/components/DoctorList.js
+import React from "react";
+import doctors from "../data/doctors";
+
+const DoctorList = ({ onSelectDoctor }) => {
+    return (
+        <div>
+            <h2>Doctors</h2>
+            <ul>
+                {doctors.map((doctor) => (
+                    <li key={doctor.id} onClick={() => onSelectDoctor(doctor)}>
+                        {doctor.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default DoctorList;
