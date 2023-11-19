a2c361e94dad56f5fc2c2f470f3f0bd2f51a7f55
Kevin Bai <lkevin957@gmail.com>
2023-11-19 10:02:48 -0800

    doctors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from "react";
+import DoctorList from "./components/DoctorList";
+import AppointmentForm from "./components/AppointmentForm";
+import AppointmentList from "./components/AppointmentList";
 
-function App() {
+const App = () => {
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+  const [appointments, setAppointments] = useState([]);
+
+  const handleSelectDoctor = (doctor) => {
+    setSelectedDoctor(doctor);
+  };
+
+  const handleRequestAppointment = (appointment) => {
+    setAppointments([...appointments, appointment]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pharmaceutical Appointments</h1>
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <DoctorList onSelectDoctor={handleSelectDoctor} />
+        </div>
+        <div style={{ flex: 1 }}>
+          {selectedDoctor && (
+            <AppointmentForm
+              selectedDoctor={selectedDoctor}
+              onRequestAppointment={handleRequestAppointment}
+            />
+          )}
+        </div>
+      </div>
+      <AppointmentList appointments={appointments} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data/doctors.js
@@ -0,0 +1,15 @@
+// src/data/doctors.js
+const doctors = [
+    { id: 1, name: "Dr. Johnson", availability: ["9:00 AM", "1:00 PM", "3:30 PM"] },
+    { id: 2, name: "Dr. Rodriguez", availability: ["10:30 AM", "2:30 PM", "4:00 PM"] },
+    { id: 3, name: "Dr. Williams", availability: ["8:00 AM", "12:00 PM", "5:00 PM"] },
+    { id: 4, name: "Dr. Chen", availability: ["11:00 AM", "3:00 PM", "4:30 PM"] },
+    { id: 5, name: "Dr. Patel", availability: ["9:30 AM", "1:30 PM", "3:45 PM"] },
+    { id: 6, name: "Dr. Davis", availability: ["8:30 AM", "12:30 PM", "4:15 PM"] },
+    { id: 7, name: "Dr. Smith", availability: ["10:00 AM", "2:00 PM", "4:45 PM"] },
+    { id: 8, name: "Dr. Kim", availability: ["11:30 AM", "3:30 PM", "5:15 PM"] },
+    { id: 9, name: "Dr. Turner", availability: ["9:45 AM", "1:45 PM", "4:15 PM"] },
+    { id: 10, name: "Dr. Mitchell", availability: ["10:45 AM", "2:45 PM", "5:30 PM"] },
+];
+
+export default doctors;
